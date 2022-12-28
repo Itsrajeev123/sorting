16272452d681a3cd81c991ca02db7efaef0f005f
Rajeev mishra <113230206+Itsrajeev123@users.noreply.github.com>
2022-12-28 21:38:34 +0530

    Update mergesortedarray.cpp

--- mergesortedarray.cpp
+++ mergesortedarray.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> nums3;
+        vector<int> ans;
         int i=0;
         int j=0;
-        int k=0;
         while(i<m && j<n){
             if(nums1[i]<nums2[j]){
-               nums3[k]=nums1[i];
-               k++;
+               ans.push_back(nums1[i]);
                i++;
             }
             else{
-               nums3[k]=nums2[j];
-               k++;
+               ans.push_back(nums2[j]);
                j++;
             }
         
         }
+
+
         while(i<m){
-              nums3[k]=nums1[i];
-               k++;
+            ans.push_back(nums1[i]);
                i++;
         }
 
         while(j<n){
-            nums3[k]=nums2[j];
+          ans.push_back(nums2[j]);
                j++;
-               k++;
          
         }
-        nums1=nums3;
+        nums1=ans;
+
     }
+    
 };
